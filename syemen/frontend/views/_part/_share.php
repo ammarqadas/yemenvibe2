4d052715161f14308928492943234cb57ae8fddd
takemetaxi <takemeye.developer@gmail.com>
2022-12-19 11:53:56 +0300

    fix image width height

--- syemen/frontend/views/_part/_share.php
+++ syemen/frontend/views/_part/_share.php
@@ -2,5 +2,5 @@
  <span  onclick="wp('https://twitter.com/share?url=<?=$turl?>');" class="i-t"></span>
 <span onclick="wp('https://www.facebook.com/sharer.php?u=<?=$turl?>')" class="i-f"></span>
  <span  onclick="wp('whatsapp://send?text=<?=$title?> <?=$url?>')" class="i-w ws"></span>
- <span onclick="wp('https://telegram.me/share/url?url=<?=$url?>')"   class="ws i-tg"></span>
+ <span onclick="wp('https://telegram.me/share/url?url=<?=$url?>')"   class="i-tg ws"></span>
 </div>
