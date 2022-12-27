d0d7d15a3c54d00b76b15245a789c57d4af3a507
takemetaxi <takemeye.developer@gmail.com>
2022-12-27 10:31:55 +0300

    wallet holder

--- syemen/frontend/views/_part/_social.php
+++ syemen/frontend/views/_part/_social.php
@@ -1,13 +1,13 @@
       <div>
-	<h4 style="color:red;font-weight:bold">أرسل هذا الخبر لأصدقائك على</h4>   
+			<p style="color:red;font-weight:bold">أرسل هذا الخبر لأصدقائك على</p>   
 
 		<div class="sm" style="font-size:2.5em">
-			 <a href="javascript:void(0)" onclick="wp('https://twitter.com/share?url=<?=$turl?>');" target="_blank"><span class="i-t"></span></a>
-			 <a href="javascript:void(0)" onclick="wp('https://www.facebook.com/sharer.php?u=<?=$turl?>')" target="_blank"> <span class="i-f"></span></a>
-			 <a href="javascript:void(0)" onclick="wp('whatsapp://send?text=<?=$title?> <?=$url?>')" class="ws"><span class="i-w"></span></a>
-			 <a href="javascript:void(0)" onclick="wp('https://telegram.me/share/url?url=<?=$url?>')"  class="ws"><span class="i-tg"></span></a>
-
+		 <span  onclick="wp('https://twitter.com/share?url=<?=$turl?>');" class="i-t"></span>
+	 	<span onclick="wp('https://www.facebook.com/sharer.php?u=<?=$turl?>')" class="i-f"></span>
+        <span  onclick="wp('whatsapp://send?text=<?=$title?> <?=$url?>')" class="i-w ws"></span>
+         <span onclick="wp('https://telegram.me/share/url?url=<?=$url?>')"   class="i-tg ws"></span>
 		</div>
 		</div>
   
 
+		
